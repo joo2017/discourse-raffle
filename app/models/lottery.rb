5993f1e6bcb4da22ea122cee49145db77172e54e
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-17 01:28:59 +0900

    Update lottery.rb

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -1,13 +1,10 @@
+# 文件名: discourse-lottery/app/models/lottery.rb
+
 # frozen_string_literal: true
 
 class Lottery < ActiveRecord::Base
-  # 修正关联关系
-  # 我们明确告诉 Rails, user 的关联是通过 'created_by_id' 这个外键
-  # 并且这个关联的名字叫做 'created_by'
-  belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
-  
-  # 我们保留这个，因为它依然很有用
   belongs_to :topic
+  belongs_to :user
 
   enum :status, {
     running: 'running',
