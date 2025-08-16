8974aac78a9b2d3be1f18cf7aeb5a93f770a0118
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-17 00:23:21 +0900

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# name: discourse-lottery
+# about: A plugin to create and manage lotteries in Discourse.
+# version: 0.1
+# authors: [Your Name]
+# url: [Your Github URL]
+
+enabled_site_setting :lottery_enabled # 这是一个示例，我们稍后会用到
+
+after_initialize do
+  # 这里将是我们添加后端逻辑的地方
+end
