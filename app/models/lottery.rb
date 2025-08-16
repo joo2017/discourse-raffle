ff381cff473cae85c8edfc7c437e167c21435a20
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-17 01:32:33 +0900

    Update lottery.rb

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -1,14 +1,6 @@
-# 文件名: discourse-lottery/app/models/lottery.rb
-
 # frozen_string_literal: true
-
 class Lottery < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
-
-  enum :status, {
-    running: 'running',
-    finished: 'finished',
-    cancelled: 'cancelled'
-  }
+  enum :status, { running: 'running', finished: 'finished', cancelled: 'cancelled' }
 end
