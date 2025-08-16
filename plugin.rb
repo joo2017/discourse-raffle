c9c71e0feecc2d521be4893cfc3ede941d5d9ded
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-17 01:01:02 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -4,8 +4,15 @@
 # authors: [Your Name]
 # url: [Your Github URL]
 
-enabled_site_setting :lottery_enabled # 这是一个示例，我们稍后会用到
+enabled_site_setting :lottery_enabled
 
 after_initialize do
-  # 这里将是我们添加后端逻辑的地方
+  # frozen_string_literal: true
+
+  # 这是我们新增的关键部分
+  # 它告诉 Discourse 在初始化之后, 去加载我们插件内部的模型文件。
+  # File.expand_path 会获取当前文件的绝对路径, 从而准确定位到模型文件。
+  load File.expand_path('../app/models/lottery.rb', __FILE__)
+
+  # 我们未来还会在这里加载 Service, Controller 等其他文件
 end
