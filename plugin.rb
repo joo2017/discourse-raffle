e562abc0a12931a1cf85f1ad019b82b62cbd1c52
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-17 02:07:32 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,18 +1,13 @@
-# name: discourse-lottery
-# about: A plugin to create and manage lotteries in Discourse.
+# name: discourse-raffle
+# about: A plugin to create and manage raffles in Discourse.
 # version: 0.1
 # authors: [Your Name]
 # url: [Your Github URL]
 
-enabled_site_setting :lottery_enabled
+enabled_site_setting :raffle_enabled
 
 after_initialize do
   # frozen_string_literal: true
 
-  # 这是我们新增的关键部分
-  # 它告诉 Discourse 在初始化之后, 去加载我们插件内部的模型文件。
-  # File.expand_path 会获取当前文件的绝对路径, 从而准确定位到模型文件。
-  load File.expand_path('../app/models/lottery.rb', __FILE__)
-
-  # 我们未来还会在这里加载 Service, Controller 等其他文件
+  load File.expand_path('../app/models/raffle.rb', __FILE__)
 end
